8ce338d00716116da51d8fd9c8cd0f4c95e17e4b
electro shop <2001fffec@gmail.com>
2026-07-05 07:52:54 +0400

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-return "Мой Flask сервер работает!"
+    return "Мой Flask сервер работает!"
 
 @app.route("/greet")
 def greet():
-name = request.args.get("name", "Незнакомец")
-return jsonify({"message": "Привет, " + name + "!"})
+    name = request.args.get("name", "Незнакомец")
+    return jsonify({"message": "Привет, " + name + "!"})
 
 if __name__ == "__main__":
 app.run()
