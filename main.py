35d98f1bd6827a281005091cad090dda0783adbd
electro shop <2001fffec@gmail.com>
2026-07-05 07:50:22 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-print("hello!I work on server ")
-print("server is started")
-print("version 2 - update")
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+return "Мой Flask сервер работает!"
+
+@app.route("/greet")
+def greet():
+name = request.args.get("name", "Незнакомец")
+return jsonify({"message": "Привет, " + name + "!"})
+
+if __name__ == "__main__":
+app.run()
+
